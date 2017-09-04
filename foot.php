76a049b192f5d258dacf403a9dc56a8eb1f394ee
BaiTang <295168799@qq.com>
2017-09-04 21:56:35 +0800

    细节

--- foot.php
+++ foot.php
@@ -6,8 +6,8 @@
         <li class="ico_2" onclick="window.location.href='fxlist.php'" style="width:33.3%">
             <span class="ico"><img src="images/wz.png"/></span><span class="txt">文章管理</span>
         </li>
-        <li class="ico_2" onclick="window.location.href='logout.php'" style="width:33.3%">
-            <span class="ico"><img src="images/wz.png"/></span><span class="txt">退出登录</span>
+        <li class="ico_3" onclick="window.location.href='logout.php'" style="width:33.3%">
+            <span class="ico"><img src="images/hy.png"/></span><span class="txt">退出登录</span>
         </li>
     </ul>
 </div>
